46811bca9ba81bbb6b8ca51afbd3775f78686c8f
Hillary Yeraldy Carpio Morales <yeraldycarpio_morales@hotmail.com>
2024-04-08 11:25:08 -0600

    capa logica de negocio

---
+++ N20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using N20240408.AccesoADatos;
+
+namespace N20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddScoped<PersonasBL>();
+            return services;
+        }
+    }
+}
---
+++ N20240408.LogicaDeNegocio/PersonasBL.cs
@@ -0,0 +1,38 @@
+﻿using N20240408.AccesoADatos;
+using N20240408.EntidadesDeNegocio;
+
+namespace N20240408.LogicaDeNegocio
+{
+    public class PersonasBL
+    {
+        readonly PersonasDAL _personasDAL;
+        public PersonasBL(PersonasDAL PersonasDAL)
+        {
+            _personasDAL = PersonasDAL;
+        }
+        public async Task<int> Crear(PersonasN personas)
+        {
+            return await _personasDAL.Crear(personas);
+        }
+        public async Task<int> Modificar(PersonasN personas)
+        {
+            return await _personasDAL.Modificar(personas);
+        }
+        public async Task<int> Eliminar(PersonasN personas)
+        {
+            return await _personasDAL.Eliminar(personas);
+        }
+        public async Task<PersonasN> ObtenerPorId(PersonasN personas)
+        {
+            return await _personasDAL.ObtenerPorId(personas);
+        }
+        public async Task<List<PersonasN>> ObtenerTodos()
+        {
+            return await _personasDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonasN>> Buscar(PersonasN personas)
+        {
+            return await _personasDAL.Buscar(personas);
+        }
+    }
+}
